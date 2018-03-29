0abc0fbcdf6900fdc00ae67d331f117c38c8f790
Dmytro Florynskyi <dmytro.florynskyi@gmail.com>
2018-03-29 17:56:22 +0300

    Added identityServer4 access to api. Added test controller and check access to auth controller

--- Colibri.Survey/Survey.Webapi/Startup.cs
+++ Colibri.Survey/Survey.Webapi/Startup.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace Survey.Webapi
 {
@@ -21,12 +24,39 @@ namespace Survey.Webapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            // Add Cors support to the service
+            services.AddCors();
+            //var policy = new Microsoft.AspNetCore.Cors.Infrastructure.CorsPolicy();
+            //policy.Headers.Add("*");
+            //policy.Methods.Add("*");
+            //policy.Origins.Add("*");
+            //policy.SupportsCredentials = true;
+            //services.AddCors(x => x.AddPolicy("corsGlobalPolicy", policy));
+
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+             .AddIdentityServerAuthentication(options =>
+             {
+                 options.Authority = "http://localhost:5050/";
+                 options.ApiName = "dataEventRecords";
+                 options.ApiSecret = "dataEventRecordsSecret";
+             });
+
+            //services.AddAuthorization();
+
+            services.AddMvc()
+            .AddControllersAsServices()
+            .AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
@@ -36,9 +66,21 @@ namespace Survey.Webapi
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
             app.UseStaticFiles();
-
+            //app.UseCors("corsGlobalPolicy");
+            //app.UseCors(builder =>builder
+            //    .AllowAnyOrigin()
+            //    .AllowAnyHeader()
+            //    .AllowAnyMethod()
+            //    .AllowCredentials()
+            //);
+            app.UseCors( options => options
+                .WithOrigins("http://localhost:8082")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+           );
+            app.UseAuthentication();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
