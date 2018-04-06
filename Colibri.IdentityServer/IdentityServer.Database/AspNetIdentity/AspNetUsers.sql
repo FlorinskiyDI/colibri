90489314341160ac4c997eb17403473ac0f2f8ae
Dmytro Florynskyi <dmytro.florynskyi@gmail.com>
2018-04-06 13:56:58 +0300

    Added scripts for groups and aspnetidentity

---
+++ Colibri.IdentityServer/IdentityServer.Database/AspNetIdentity/AspNetUsers.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE [dbo].[AspNetUsers] (
+    [Id]                   NVARCHAR (450)     NOT NULL,
+    [AccessFailedCount]    INT                NOT NULL,
+    [AccountExpires]       DATETIME2 (7)      NOT NULL,
+    [ConcurrencyStamp]     NVARCHAR (MAX)     NULL,
+    [DataEventRecordsRole] NVARCHAR (MAX)     NULL,
+    [Email]                NVARCHAR (256)     NULL,
+    [EmailConfirmed]       BIT                NOT NULL,
+    [IsAdmin]              BIT                NOT NULL,
+    [LockoutEnabled]       BIT                NOT NULL,
+    [LockoutEnd]           DATETIMEOFFSET (7) NULL,
+    [NormalizedEmail]      NVARCHAR (256)     NULL,
+    [NormalizedUserName]   NVARCHAR (256)     NULL,
+    [PasswordHash]         NVARCHAR (MAX)     NULL,
+    [PhoneNumber]          NVARCHAR (MAX)     NULL,
+    [PhoneNumberConfirmed] BIT                NOT NULL,
+    [SecuredFilesRole]     NVARCHAR (MAX)     NULL,
+    [SecurityStamp]        NVARCHAR (MAX)     NULL,
+    [TwoFactorEnabled]     BIT                NOT NULL,
+    [UserName]             NVARCHAR (256)     NULL,
+	[GroupId] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+
+	CONSTRAINT [FK_Subscriptions_ToSubscriptionTypes] FOREIGN KEY ([GroupId]) REFERENCES [dbo].[Groups]([Id]),
+    CONSTRAINT [PK_AspNetUsers] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [EmailIndex]
+    ON [dbo].[AspNetUsers]([NormalizedEmail] ASC);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UserNameIndex]
+    ON [dbo].[AspNetUsers]([NormalizedUserName] ASC) WHERE ([NormalizedUserName] IS NOT NULL);
+
