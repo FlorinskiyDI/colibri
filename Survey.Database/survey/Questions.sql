888144bc6389b8d2f3adca9a11ef9efbed592508
Anton Butenko <nvdevelop@windowslive.com>
2018-05-11 17:22:12 +0300

    Add queries for create survey db

---
+++ Survey.Database/survey/Questions.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Questions] (
+    [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT (NEWSEQUENTIALID()), 
+	[Parent _Id] UNIQUEIDENTIFIER NULL, 
+    [Name] NVARCHAR(250) NOT NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+	[Required] BIT NOT NULL DEFAULT 0,
+	[OrderNo] INT NOT NULL, 
+	[AdditionalAnswer] BIT NOT NULL DEFAULT 0,
+	[AllowMultipleOptionAnswers] BIT NOT NULL DEFAULT 0,
+	[InputTypes_Id]  UNIQUEIDENTIFIER NOT NULL, 
+	[Page_Id]  UNIQUEIDENTIFIER NOT NULL, 
+	[OptionGroup_Id]  UNIQUEIDENTIFIER NOT NULL, 
+
+
+	CONSTRAINT [FK_Questions_InputTypes] FOREIGN KEY ([InputTypes_Id]) REFERENCES [dbo].[InputTypes]([Id]),
+	CONSTRAINT [FK_Questions_Pages] FOREIGN KEY ([Page_Id]) REFERENCES [dbo].[Pages]([Id]),
+	CONSTRAINT [FK_Questions_OptionGroups] FOREIGN KEY ([OptionGroup_Id]) REFERENCES [dbo].[OptionGroups]([Id]),
+
+	CONSTRAINT [FK_Questions_Questions] FOREIGN KEY ([Parent _Id]) REFERENCES [dbo].[Questions]([Id]),
+);
