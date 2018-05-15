8df6abcceed38b3f982bbbb04fd5bf63cd1a92b3
Dmytro Florynskyi <dmytro.florynskyi@gmail.com>
2018-05-15 20:37:30 +0300

    Added base Repository and uow pattern

---
+++ Colibri.Survey/storagecore.EntityFrameworkCore/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using storagecore.Abstractions.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace storagecore.EntityFrameworkCore.Entities
+{
+    public class BaseEntity<TKey> : IBaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+
+    public class BaseTenantEntity<TKey> : BaseEntity<TKey>, IBaseEntity<TKey>
+    {
+        public TKey TenantId { get; set; }
+    }
+}
